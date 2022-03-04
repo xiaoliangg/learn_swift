4f87f6ef53b0a44830729197b0b38b93bcfbc103
xiaoliangg <xiaoliangyuu@163.com>
2022-03-04 22:38:44 +0800

    001 可选类型细讲 ??和?的使用

--- 001_hello.playground/Contents.swift
+++ 001_hello.playground/Contents.swift
@@ -40,3 +40,14 @@ var s6 = Int("ss") ?? 11
 print("可选类型s6:\(s6)")
 var s7 = Int("12") ?? 11
 print("可选类型s6:\(s7)")
+
+print("-----可选类型Optional细讲-------")
+//var opt1 = nil // 直接声明nil会报错
+var opt1:Int? = nil
+print(opt1)
+print(opt1 ?? 11)
+//print(opt1!) // nil类型强转报错
+var opt2:Int? = 20
+print(opt2)
+print(opt2 ?? 11)
+print(opt2!)
