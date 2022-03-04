fb4bd53a9e80daae6e2b2a2ef2a6e1e99d6b7284
xiaoliangg <xiaoliangyuu@163.com>
2022-03-04 22:17:04 +0800

    001 基本类型

--- 001_hello.playground/Contents.swift
+++ 001_hello.playground/Contents.swift
@@ -1,5 +1,23 @@
 import UIKit
 
 var greeting = "Hello, playground"
-
 print(greeting)
+
+// 基本类型声明和定义
+var s1:String = "String test"
+print(s1);
+var i:Int = 32;
+print(i);
+var f:Float = 32.00;
+print(f)
+var c:Character = "c"
+print(c);
+
+// 基本类型声明和定义
+var s2 = "String test2"
+print(s2);
+
+// 常量
+let i3 = 30
+//i3=20
+print(i3)
