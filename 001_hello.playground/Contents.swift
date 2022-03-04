f52da2bcf19d6c64994b589569c300bdd72c227f
xiaoliangg <xiaoliangyuu@163.com>
2022-03-04 22:24:05 +0800

    001 类型别名typealias 和类型转换

--- 001_hello.playground/Contents.swift
+++ 001_hello.playground/Contents.swift
@@ -13,12 +13,22 @@ print(f)
 var c:Character = "c"
 print(c);
 
-// 基本类型声明和定义
 var s2 = "String test2"
 print(s2);
 
-// 常量
+print("-----常量-------")
 let i3 = 30
 //i3=20
 print(i3)
 print("i3:\(i3)")
+
+print("-----类型别名-------")
+typealias aa=Int
+var a:aa=32
+print("a:\(a)")
+print(type(of: a))
+
+print("-----类型转换-------")
+var a3 = 10
+print("值=" + String(a3))
+print("值=" + String(true))
