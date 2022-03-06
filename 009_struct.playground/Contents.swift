fe3012e96fd2d1df3232d6f51ae7af92afc8c6fa
xiaoliangg <xiaoliangyuu@163.com>
2022-03-06 18:33:16 +0800

    009 结构体:计算属性-只读属性的初始化;newValue的使用

--- 009_struct.playground/Contents.swift
+++ 009_struct.playground/Contents.swift
@@ -49,5 +49,39 @@ struct Person2
 }
 
 var person2 = Person2()
-//person2.name = "name_test" // 编译报错
+//person2.name = "name_test" // 编译报错:cannot assign to property: 'name' is a get-only property
 print(person2.name)
+
+print("---------结构体:计算属性-只读属性的初始化-----------")
+struct Person3
+{
+    private var testValue = "test-read-only"
+    private var name:String
+    {
+        set{
+            // newValue是默认属性，set参数可以不用写
+            testValue = newValue
+        }
+        get
+        {
+            return testValue
+        }
+    }
+    
+    init()
+    {
+
+    }
+    init(name:String)
+    {
+        self.name = name
+    }
+    
+    // testValue和name都是私有的，只能通过此方法读取属性
+    func getName() -> String{
+        return self.name
+    }
+}
+
+var person3 = Person3(name: "init test")
+print(person3.getName())
