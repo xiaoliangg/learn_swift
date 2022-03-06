af1956796c5bfc5b591009feb88b2d641015a431
xiaoliangg <xiaoliangyuu@163.com>
2022-03-06 12:37:38 +0800

    003 函数实参outname，形参inname

--- 003_function.playground/Contents.swift
+++ 003_function.playground/Contents.swift
@@ -10,3 +10,10 @@ func test1(name:String...)
 }
 
 test1(name:"a","b")
+
+print("-----------函数实参outname，形参inname-----------")
+func test2(outname inname:String)
+{
+    print(inname)
+}
+test2(outname: "name")
