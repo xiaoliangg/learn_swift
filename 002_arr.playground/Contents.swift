f0e7ac496b5e5cc88043bccd0104072e1f7bdc31
xiaoliangg <xiaoliangyuu@163.com>
2022-03-06 11:42:35 +0800

    002 可选项绑定:把一个可选类型赋给非可选类型时做判断

--- 002_arr.playground/Contents.swift
+++ 002_arr.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Darwin
 
 print("-----元组作为函数的参数和返回值-------")
 var arr = (name1:1,name2:"name2ss",name3:1.5)
@@ -16,3 +17,16 @@ print(arr)
 print("-------------------")
 let arr2 = handle(param: arr)
 print(arr2)
+
+
+
+print("-----可选项绑定:把一个可选类型赋给非可选类型时做判断-------")
+var a:Int? = 10
+
+if var b = a
+{
+    print("a有值")
+}else{
+    print("a没有值")
+}
+//print(b??0)  //此行会报错  Cannot find 'b' in scope
