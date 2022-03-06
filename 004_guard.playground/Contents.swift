34f3c3b7af6fbc8e270e88091bebf39138d0886c
xiaoliangg <xiaoliangyuu@163.com>
2022-03-06 12:59:31 +0800

    004 guard与可选类型转换搭配使用

--- 004_guard.playground/Contents.swift
+++ 004_guard.playground/Contents.swift
@@ -15,3 +15,18 @@ func test1(param:Int)
 test1(param: 5)
 test1(param: 11)
 
+print("---------guard与可选类型转换搭配使用----------")
+var a:Int? = 10;
+var c:Int? = nil;
+func test2(param:Int?)
+{
+    guard let b = param else
+    {
+        print("进入guard语句:\(param)")
+        return
+    }
+    print("hello world:\(b)")
+    return
+}
+test2(param: a)
+test2(param: c)
