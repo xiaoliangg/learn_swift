7b71f25e651a734baf7b04f1fa4e9fcd688e7360
xiaoliangg <xiaoliangyuu@163.com>
2022-03-06 15:31:04 +0800

    008 结构体:关键字 mutating 修改属性

---
+++ 008_struct.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+print("---------结构体:关键字 mutating 修改属性")
+struct Student
+{
+    var name = "unknown"
+    var score = 0
+    var age = 0
+    var isPass = false
+    
+    static let scholName = "家里蹲大学"
+    
+    init(){
+        
+    }
+    init(name:String,score:Int,age:Int){
+        self.name = name
+        self.score = score
+        self.age = age
+        self.isPass = score >= 60
+    }
+    
+    func getName() -> String{
+        return self.name
+    }
+    mutating func setName(name:String){
+        self.name = name; // 不使用 mutating 关键字，会报错:Cannot assign to property: 'self' is immutable
+    }
+}
