203203045ca350fa9093f68a52b8ddb4cf3f256b
xiaoliangg <xiaoliangyuu@163.com>
2022-03-06 12:39:24 +0800

    003 函数参数:只写一个表示的外部名称，默认内部名称和外部名称一样

--- 003_function.playground/Contents.swift
+++ 003_function.playground/Contents.swift
@@ -12,6 +12,7 @@ func test1(name:String...)
 test1(name:"a","b")
 
 print("-----------函数实参outname，形参inname-----------")
+print("-----------函数参数:只写一个表示的外部名称，默认内部名称和外部名称一样-----------")
 func test2(outname inname:String)
 {
     print(inname)
