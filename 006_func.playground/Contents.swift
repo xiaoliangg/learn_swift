cd94738afb1ea01042808779beab8cb981496313
xiaoliangg <xiaoliangyuu@163.com>
2022-03-06 14:03:50 +0800

    006 函数:内嵌函数

--- 006_func.playground/Contents.swift
+++ 006_func.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-print("-----------------函数类型作为返回值-----------------")
+print("-----------------函数:函数类型作为返回值-----------------")
 func play1(param:Int) -> Int
 {
     return param * param;
@@ -17,3 +17,22 @@ func test1(param:Bool) -> (Int) ->Int
 }
 var fun_result = test1(param: false)
 print(fun_result(3))
+
+print("-----------------函数:内嵌函数-----------------")
+
+func test2(param:Bool) -> (Int) ->Int
+{
+    func play3(param:Int) -> Int
+    {
+        return param * param;
+    }
+
+    func play4(param:Int) -> Int
+    {
+        return param + param;
+    }
+    
+    return param ? play3 : play4
+}
+var fun_result2 = test1(param: false)
+print(fun_result2(4))
