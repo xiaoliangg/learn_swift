b27f5d44e8681b311b2cf0ae3a7cb9113a4f1346
xiaoliangg <xiaoliangyuu@163.com>
2022-03-15 21:27:25 +0800

    012 class:关键字 convenience 调用其他构造方法

--- 012_class.playground/Contents.swift
+++ 012_class.playground/Contents.swift
@@ -1,3 +1,42 @@
 import UIKit
 
-var greeting = "Hello, playground"
+print("---------class:关键字 convenience 调用其他构造方法-------------")
+class Student
+{
+    var name = "unknow"
+    var age = 0
+    var score = 0.0
+    var isPass = false
+    
+    static let schoolName = "家里蹲大学"
+    
+    convenience init()
+    {
+        self.init(name: "default", age: 1, score: 1.0)
+    }
+    
+    init(name:String,age:Int,score:Double)
+    {
+        self.name = name;
+        self.age = age;
+        self.score = score;
+        self.isPass = score >= 60
+    }
+    
+    func getIsPass() -> Bool
+    {
+        return self.isPass
+    }
+    
+    func getName() -> String
+    {
+        return self.name
+    }
+}
+
+//var student = Student(name: "liang", age: 16, score: 51.0)
+//print(student.getIsPass())
+
+var student1 = Student()
+print(type(of: student1))
+print(student1.getName())
