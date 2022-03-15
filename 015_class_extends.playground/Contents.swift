16d22e3721dffb503ced5051f9f352a3d01938a3
xiaoliangg <xiaoliangyuu@163.com>
2022-03-15 22:35:42 +0800

    015 class_extends:向下类型转换的几种使用方式

---
+++ 015_class_extends.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+class Person
+{
+    private var name:String
+    private var age:Int
+    init(name:String,age:Int)
+    {
+        self.name = name
+        self.age = age
+    }
+    public func setName(name:String)
+    {
+        self.name = name
+    }
+    public func setAge(age:Int)
+    {
+        self.age = age
+    }
+    public func getName() ->String
+    {
+        return self.name
+    }
+    public func getAge() ->Int
+    {
+        return self.age
+    }
+}
+class Student:Person
+{
+    public func play()
+    {
+        print("\(self.getName())正中玩儿")
+    }
+}
+print("---------class_extends:继承的一般使用-------------")
+var student = Student(name: "studentA", age: 16)
+print("student_name:" + student.getName())
+student.play()
+
+
+print("---------class_extends:向下类型转换-------------")
+var student2:Person = Student(name: "studentA", age: 16)
+print("student_name:" + student2.getName())
+print(type(of: student2))
+print("---------class_extends:Optional调用-------------")
+var student3 = student2 as? Student
+print(type(of: student3))
+student3?.play() // Optional调用需要加问号
+student3!.play()
+print("---------class_extends:可选项绑定-------------")
+if let student4 = student2 as? Student{
+    print(type(of: student4))
+    student4.play()
+}
+print("---------class_extends:强转-------------")
+var student5 = student2 as! Student
+print(type(of: student5))
+student5.play()
