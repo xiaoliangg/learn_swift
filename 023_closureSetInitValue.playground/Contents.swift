ea5e84b42356b0b72c6669c0c1dcd91be853f9a0
xiaoliangg <xiaoliangyuu@163.com>
2022-03-20 13:51:47 +0800

    023 deinit:可选对象才可置为空

--- 023_closureSetInitValue.playground/Contents.swift
+++ 023_closureSetInitValue.playground/Contents.swift
@@ -18,10 +18,11 @@ class Test
     }
 }
 
-var test = Test()
-print(test.name)
-print(test.age)
+var test:Test? = Test()
+print(test!.name)
+print(test!.age)
 
 print("---------deinit:反初始化器-------------")
 // 创建一个新对象，自动调用反初始化器
 test = Test()
+test = nil
