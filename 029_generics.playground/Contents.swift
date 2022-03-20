ca51b146161684fb8d44e76c44f33055586b404e
xiaoliangg <xiaoliangyuu@163.com>
2022-03-20 22:08:44 +0800

    029 generics:协议里的泛型类型限定-associatedtype关键字

--- 029_generics.playground/Contents.swift
+++ 029_generics.playground/Contents.swift
@@ -18,3 +18,31 @@ func play<T:Data>(param:T) -> String
 }
 
 print(play(param: Data(name: "hello")))
+
+print("---------generics:协议里的泛型类型限定-associatedtype关键字-------------")
+protocol Test
+{
+    // 也可以 泛型类型限定
+//    associatedtype D:Data
+    associatedtype D
+
+    func play(param:D)
+    
+    func play2(param1:Int,param2:D)
+}
+
+class Student:Test
+{
+    
+    // play方法的参数类型和play2方法的param2类型应该相同，否则报错
+    func play(param: String) {
+        print(param)
+    }
+    
+    func play2(param1: Int, param2: String) {
+        print(param2)
+    }
+}
+var student = Student()
+student.play(param: "hello")
+student.play2(param1: 1, param2: "world")
