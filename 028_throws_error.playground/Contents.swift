ce85e2eda85b153edb0018c8895139929b262016
xiaoliangg <xiaoliangyuu@163.com>
2022-03-20 18:22:15 +0800

    028 throws_error:try-catch写法及测试

--- 028_throws_error.playground/Contents.swift
+++ 028_throws_error.playground/Contents.swift
@@ -9,7 +9,7 @@ enum TestError:String,Error
 }
 
 // throws 标记方法可能跑出错误
-func play1(param:Int) throws
+func play1(param:Int) throws -> String
 {
     if(param < 0)
     {
@@ -18,6 +18,7 @@ func play1(param:Int) throws
     {
         throw TestError.error2
     }
+    return "hello world"
 }
 
 // 直接调用可能抛出异常的方法会有以下三种提示
@@ -29,7 +30,7 @@ func play1(param:Int) throws
 
 // 按照提示fix后，以下三种写法
 // Did you mean to use 'try'?
-//try play1(param: -1) // 如果抛出异常，会停止执行并向上抛出异常
+try play1(param: 12) // 如果抛出异常，会停止执行并向上抛出异常
 print("分隔线一")
 
 // Did you mean to handle error as optional value?
@@ -39,3 +40,26 @@ print("分隔线二") // 如果抛出异常，会继续执行
 // Did you mean to disable error propagation?
 try! play1(param: 12) // 确保不会抛出异常时使用这种
 print("分隔线三")
+
+
+print("---------throws_error:try-catch写法-------------")
+do{
+//    try play1(param: -1) // 抛出error1
+//    try play1(param: 5) // 抛出error2
+    try play1(param: 12) // 不抛出异常
+    
+    var value = try play1(param: 15) // try关键字写在语句里
+    print(value)
+}catch(TestError.error1)
+{
+    print("抛出error1")
+}catch(TestError.error2)
+{
+    print("抛出error2")
+}
+// 类似finally
+defer{
+    print("执行defer")
+}
+
+print("分隔线四")
